bcc6c13950544d69a2a9cef3867b096d690af24a
tsuncoder <thiensoncva01@gmail.com>
2025-02-21 19:49:16 +0700

    fix import

--- src/core/config/env.validation.ts
+++ src/core/config/env.validation.ts
@@ -1,12 +1,12 @@
 import * as Joi from 'joi';
-import { APP_CONSTANTS } from './constants';
+import { APP_CONFIG_CONSTANT } from './constants';
 
 export const envValidationSchema = Joi.object({
   NODE_ENV: Joi.string()
     .valid(
-      APP_CONSTANTS.NODE_ENV.development,
-      APP_CONSTANTS.NODE_ENV.production,
-      APP_CONSTANTS.NODE_ENV.test,
+      APP_CONFIG_CONSTANT.NODE_ENV.development,
+      APP_CONFIG_CONSTANT.NODE_ENV.production,
+      APP_CONFIG_CONSTANT.NODE_ENV.test,
     )
     .required(),
   PORT: Joi.number().default(3000),
